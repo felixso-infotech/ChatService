98d0ec1e70bbd19d6c4a1fe5cc3ef5ffe2e2b992
AnjaliLxi <anjali.chandran@lxisoft.com>
2019-09-19 16:53:26 +0530

    KAR-172 KAR-174 refactoring and checking issues

--- src/main/java/com/felixsoinfotech/chat/XmppChatApplication/service/XmppService.java
+++ src/main/java/com/felixsoinfotech/chat/XmppChatApplication/service/XmppService.java
@@ -20,6 +20,7 @@ package com.felixsoinfotech.chat.XmppChatApplication.service;
  */
 
 import org.jivesoftware.smack.AbstractXMPPConnection;
+import org.jivesoftware.smack.MessageListener;
 import org.jivesoftware.smack.SmackException;
 import org.jivesoftware.smack.chat.ChatManagerListener;
 import org.jivesoftware.smack.chat.ChatMessageListener;
@@ -39,7 +40,7 @@ import org.springframework.stereotype.Service;
 import com.felixsoinfotech.chat.XmppChatApplication.configuration.XmppConfiguration;
 
 @Service
-public class XmppService implements ChatMessageListener  {
+public class XmppService implements MessageListener  {
 
 	public XmppService() {
 		// TODO Auto-generated constructor stub
@@ -65,10 +66,13 @@ public class XmppService implements ChatMessageListener  {
 		
 		if(chatManager!=null) {
 		try {
+			
+			logger.info("*****inside tryy");
 			Chat newChat = chatManager.chatWith(JidCreate.from("anjali@jabber.hot-chilli.net").asEntityBareJidOrThrow());
 
-		if (connection.isConnected() && connection.isAuthenticated()) {
-			newChat.send(newMessage);
+		if (connection.isConnected()) {
+			newChat.send(newMessage.getBody());
+			logger.info("message sent***{}",newMessage);
 		}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -89,14 +93,17 @@ public class XmppService implements ChatMessageListener  {
 			  chatManager = ChatManager.getInstanceFor(connection);
 			  logger.info("*****connection in get message{}",chatManager);
 			  
-			  final EntityBareJid to = newMessage.getTo().asEntityBareJidOrThrow();
-              
-			   
+			  logger.info("entitybarejid from{}",newMessage.getBody());
+			  
+			  //final EntityBareJid to = newMessage.getTo().asEntityBareJidOrThrow();
+			  final EntityBareJid from = newMessage.getFrom().asEntityBareJidOrThrow();
+	             
+			  
 			 chatManager.addIncomingListener(new IncomingChatMessageListener() {
 	        	  @Override
-	        	  public void newIncomingMessage(EntityBareJid from, Message message, Chat chat) {
-	        	      System.out.println("New message from " + from + ": " + message.getBody());
-	        	      logger.info("chat body{}",message.getBody());
+	        	  public void newIncomingMessage(EntityBareJid from, Message newMessage, Chat chat) {
+	        	      System.out.println("New message from " + from + ": " + newMessage.getBody());
+	        	      logger.info("chat body{}",newMessage.getBody());
 	        	      
 	        	  }
 	        	});
@@ -108,12 +115,17 @@ public class XmppService implements ChatMessageListener  {
 	}
 
 	/* (non-Javadoc)
-	 * @see org.jivesoftware.smack.chat.ChatMessageListener#processMessage(org.jivesoftware.smack.chat.Chat, org.jivesoftware.smack.packet.Message)
+	 * @see org.jivesoftware.smack.MessageListener#processMessage(org.jivesoftware.smack.packet.Message)
 	 */
 	@Override
-	public void processMessage(org.jivesoftware.smack.chat.Chat arg0, Message arg1) {
+	public void processMessage(Message arg0) {
 		// TODO Auto-generated method stub
 		
 	}
+
+
+	
+
+	
 
 }
