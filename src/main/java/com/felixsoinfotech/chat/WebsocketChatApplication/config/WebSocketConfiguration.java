0bc05cb499e8abf7207251899631f67636a5fe85
AnjaliLxi <anjali.chandran@lxisoft.com>
2020-01-02 00:31:39 +0530

    created chat server KAR-578 KAR-584

---
+++ src/main/java/com/felixsoinfotech/chat/WebsocketChatApplication/config/WebSocketConfiguration.java
@@ -0,0 +1,31 @@
+package com.felixsoinfotech.chat.WebsocketChatApplication.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+
+	private static final Logger logger = LoggerFactory.getLogger(WebSocketConfiguration.class);
+
+	@Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/socket")
+                .setAllowedOrigins("*")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app")
+                .enableSimpleBroker("/chat");
+    }
+    
+}
